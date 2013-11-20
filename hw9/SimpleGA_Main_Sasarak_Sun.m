628faa9e40ff777be76286f2a004c2ece6f07517
Luke Sun <luke.q.sun@mail.rit.edu>
2013-11-20 17:02:10 -0500

    Update test results for part 1, 10000

---
+++ hw9/SimpleGA_Main_Sasarak_Sun.m
@@ -0,0 +1,29 @@
+length = 10000;
+population = 200;
+generation = 20000;
+tournament = 0.7;
+probability = 0.5;
+round = 100;
+
+% Create a random population
+initPop = randi([0 1], population, length);
+% Evaluate all individuals in population
+% Maximize number represented by binary vector 
+score = sum(initPop(:, :), 2);
+% Sort population by fitness value
+lastPop = [ initPop score ];
+totalBestScores = 0;
+
+for i = 1 : generation/round    
+    if ( mod(i, round) == 0)
+        [ bestVector, bestScore, lastPop, lastBestScores ] = SimpleGA_MultipleOperator_Sasarak_Sun( length, population, 1, tournament, probability, 'UniformCrossover', 0.2, lastPop );
+    else
+        [ bestVector, bestScore, lastPop, lastBestScores ] = SimpleGA_MultipleOperator_Sasarak_Sun( length, population, round - 1, tournament, probability, 'Mutation', 1, lastPop );
+    end
+    
+    totalBestScores = [totalBestScores lastBestScores];
+    
+    disp(bestScore);
+end
+
+figure, plot(1:size(totalBestScores, 2), totalBestScores), xlabel('generation'), ylabel('performance'), title('performance vs generation');
